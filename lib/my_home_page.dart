97f976abaf2e4ebc6f874d749b35e9f784cb4f42
jiansim03 <jiansim03@gmail.com>
2023-10-25 15:31:40 +0900

    스플래시 페이지 수정
    앱 처음 구동시 나타나는 스플래시 페이지가 다소 수정되었습니다

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'onboard.dart'; // onboard 위젯을 사용하기 위한 임포트
+import 'package:flutter_spinkit/flutter_spinkit.dart'; // 로딩 애니메이션
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -17,7 +18,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _navigateToonboard() async {
-    await Future.delayed(const Duration(seconds: 5), () {
+    await Future.delayed(const Duration(seconds: 5), () { // 5초 후 onboard 위젯으로 이동
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => const onboard()));
     });
@@ -25,26 +26,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Icon(
               Icons.music_note,
-              size: 500,
+              size: 100,
+              color: Colors.white,
             ), // 큰 음표 모양
             SizedBox(
               height: 20,
             ), // 간격 조절
             Text(
-              'Your Title Here',
+              'Perfect\nBeat',
+              textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              ),
-            ), // 타이틀
-          ],
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ), // 헤드라인
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 1.0,
+              height: 100.0,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '연주한 곡이 악보와\n얼마나 비슷한지 알려드려요!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ), // 앱 부가 설명
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            SpinKitRipple(color: Colors.white),
+          ], // 로딩 애니메이션
         ),
       ),
     );
